1a442b8b54ff91572fbab74c1118827d6b428281
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-05-02 13:52:48 +0500

    Back to async

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from collections.abc import Generator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from typing import AsyncGenerator
 
 from app.core.config import settings
 
@@ -9,16 +8,16 @@ class Database:
 
         self.url = url or self._build_postgres_url()
 
-        self.engine = create_engine(
+        self.engine = create_async_engine(
             self.url,
             echo=settings.APP_DEBUG,
             future=True,
             pool_pre_ping=True
         )
 
-        self.SessionLocal = sessionmaker(
+        self.SessionLocal = async_sessionmaker(
             bind=self.engine,
-            class_=Session,
+            class_=AsyncSession,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
@@ -26,15 +25,15 @@ class Database:
         )
 
     def _build_postgres_url(self) -> str:
-        return settings.get_db_url()
+        return settings.db_url()
 
-    def get_session(self) -> Session:
+    async def get_session(self) -> AsyncSession:
         return self.SessionLocal()
 
 db=Database()
 engine = db.engine
 SessionLocal = db.SessionLocal
 
-def get_db() -> Generator[Session, None]:
+def get_db() -> AsyncGenerator[AsyncSession, None]:
     with SessionLocal() as session:
         yield session
