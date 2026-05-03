22c647a8b1377c75beb213713eba475a65939be5
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-05-03 22:57:49 +0500

    Bundle App Exceptions in core/errors. Add Exception handler.

--- app/core/errors.py
+++ app/core/errors.py
@@ -7,3 +7,32 @@ class SeedCategoryException(BaseAppException):
 	def __init__(self, detail: str = "Ошибка справочника"):
 		self.detail = detail
 		super().__init__(detail)
+
+class RequestNotFoundError(BaseAppException):
+	def __init__(self, request_id: int):
+		self.detail = f"Запрос с id = {request_id} не найден"
+		super().__init__(self.detail)
+
+
+class ClarificationNotFoundError(BaseAppException):
+	def __init__(self, request_id: int):
+		self.detail = f"Пояснение для запроса id = {request_id} не найдено"
+		super().__init__(self.detail)
+
+
+class ClarificationAlreadyAnsweredError(BaseAppException):
+	def __init__(self, clarification_id: int):
+		self.detail = f"Пояснение id = {clarification_id} уже получило ответ"
+		super().__init__(self.detail)
+
+
+class ClassificationFailedError(BaseAppException):
+	def __init__(self, request_id: int):
+		self.detail = f"Не вышло классифицировать запрос {request_id}"
+		super().__init__(self.detail)
+
+
+class PersistenceError(BaseAppException):
+	def __init__(self, detail: str = "Ошибка при попытке сохранить"):
+		self.detail = detail
+		super().__init__(detail)
--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ from app.api.analytics import analytics_router
 from app.api.requests import requests_router
 from app.core.config import settings
 from app.repos.category_repo import CategoryRepository
+from app.api.exc_handlers import app_error_handler
+from app.services.classifier import DummyClassifier
+from app.services.toxicity import DummyToxicityDetector
+from app.core.errors import BaseAppException
 
 from app.db.healthcheck import check_db
 
@@ -21,6 +25,9 @@ async def lifespan(app: FastAPI):
 		count = await check_db(session)
 		print(f"[OK] DB connected, categories loaded: {count}")
 
+	app.state.classifier = DummyClassifier()
+	app.state.toxicity_detector = DummyToxicityDetector()
+
 	yield
 
 
@@ -29,6 +36,7 @@ def create_app() -> FastAPI:
 	app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
 	app.include_router(requests_router)
 	app.include_router(analytics_router)
+	app.add_exception_handler(BaseAppException, app_error_handler)
 
 	@app.get("/health")
 	async def health():
